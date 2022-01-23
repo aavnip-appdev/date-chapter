24f8ae61a0e96e02155a14da279b5f7d675d80e9
aavnip <aavnip@chicagobooth.edu>
2022-01-23 22:35:09 +0000

    Date Completed

--- date_math.rb
+++ date_math.rb
@@ -5,3 +5,9 @@
 # Output:
 #   "Ruby is 108937 days old!"
 #
+require "date"
+ruby_date = Date.new(1995,12,21)
+p ruby_date
+ruby_age = Date.today - ruby_date
+ruby_age = ruby_age.to_i
+p "Ruby is " + ruby_age.to_s + " days old!"
